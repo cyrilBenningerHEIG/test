5aa5af62d2f9997fd291a380afa27052db018c81
loic.postec <loic.postec@heig-vd.ch>
2019-06-03 16:39:16 +0200

    build

--- resources/js/components/CarteSelectionSemaine.vue
+++ resources/js/components/CarteSelectionSemaine.vue
@@ -1,6 +1,7 @@
 <template>  
-    <!-- <div v-for="vin in vins"> -->
+
       <div class="card card-custom mx-2 mb-3" >
+            <!-- <div v-for="vin in vins"> -->
     <img src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" class="card-img-top" alt="vin1">
     <div class="card-body" id="weekproduct">
       <!-- <h5 class="card-title">{{vin.nom}}</h5>
