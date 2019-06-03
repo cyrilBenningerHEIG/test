4bec5ca7722a0d7c7a888e3384193a3fd4f27a0b
Alvaro Baptista <alvaro.baptista@heig-vd.ch>
2019-06-03 15:11:08 +0200

    insert selection semaine

--- resources/js/components/CarteSelectionSemaine.vue
+++ resources/js/components/CarteSelectionSemaine.vue
@@ -1,16 +1,18 @@
-<template>
-<!-- v-for="weekselectionproduct in weekselectionproducts" v-bind:key="weekselectionproduct_id" -->
-  
+<template>  
+    <!-- <div v-for="vin in vins"> -->
       <div class="card card-custom mx-2 mb-3" >
     <img src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" class="card-img-top" alt="vin1">
     <div class="card-body" id="weekproduct">
-      <h5 class="card-title">Nom du vin</h5>
-      <p class="card-text">Spécificités</p>
+      <!-- <h5 class="card-title">{{vin.nom}}</h5>
+      <p class="card-text">{{vin.millesime}}</p> -->
     </div>
   </div>
+  <!-- </div> -->
 </template>
 <script>
 export default {
+  props: ['vins'],
+
     data(){
         return{
             weekselection : [],
