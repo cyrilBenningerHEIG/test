73fc0229cd931d7fc04949d30078c8a16963e15a
loic.postec <loic.postec@heig-vd.ch>
2019-06-03 16:53:29 +0200

    fix view selection semaine

--- resources/js/components/CarteSelectionSemaine.vue
+++ resources/js/components/CarteSelectionSemaine.vue
@@ -1,11 +1,12 @@
 <template>  
-      <div class="card card-custom mx-2 mb-3" >
-      <div v-for="vin in vins">
+<div class="container" >
+      <div class="card card-custom" v-for="vin in vins">
     <img src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" class="card-img-top" alt="vin1">
     <div class="card-body" id="weekproduct">
       <h5 class="card-title">{{vin.nom}}</h5>
       <p class="card-text">{{vin.millesime}}</p>
     </div>
+  </div>
   </div>
   </div>
 </template>
